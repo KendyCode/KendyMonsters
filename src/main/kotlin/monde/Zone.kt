40668ecdb37e86e8ff827e063b09cdd5d102d7b0
KendyCode <kendyprox@gmail.com>
2025-09-19 17:06:59 +0200

    progresion get set pv

--- src/main/kotlin/monde/Zone.kt
+++ src/main/kotlin/monde/Zone.kt
@@ -1,8 +1,16 @@
 package monde
 
+import monstre.EspeceMonstre
+
 class Zone(
     var id : Int,
     var nom : String,
     var expZone : Int,
+    var especesMonstres: MutableList<EspeceMonstre> = mutableListOf(),
+    var zoneSuivante : Zone? = null,
+    var zonePrecedente : Zone? = null,
+    //TODO faire la méthode genereMonstre()
+    //TODO faire la méthode rencontreMonstre()
 
-)
+
+    )
